5a7b606027902c64353e64077bbf84d42ad4f628
David <david.lazaro@babelgroup.com>
2022-04-06 17:29:58 +0200

    Agregado Input y output

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,16 +1,35 @@
 import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html'
 })
-export class MainPageComponent  {
+export class MainPageComponent {
+
+  nuevo : Personaje = {
+    nombre:'Gloria Estefan',
+    poder:10
+  }
+
+personajes: Personaje[] = [
+  {
+    nombre: 'Goku',
+    poder: 15000
+},
+{
+  nombre: 'Vegeta',
+  poder: 9000
+}
+]
+
+agregarNuevoPersonaje( elemento: Personaje) {
+  this.personajes.push(elemento);
+}
 
 // Agregar(evento:SubmitEvent) {
 // evento.preventDefault();
 // }
-Agregar() {
-  
-}
+
 
 }
